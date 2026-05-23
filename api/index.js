600b5bb8f9800bdc1f1c511424328decbe441f00
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-23 11:54:11 -0300

    chore: update shareCode to PGNHQ4A9TBU

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=RA4C3KNXG2Y&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=PGNHQ4A9TBU&afp2=oa' });
     res.end();
   }
