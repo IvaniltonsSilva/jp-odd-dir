e2774b176961e66f9d81206b7e9c2c97f236af81
IvaniltonsSilva <265127343+IvaniltonsSilva@users.noreply.github.com>
2026-05-26 12:24:49 -0300

    chore: update shareCode to VYU22TH6U14

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=ZMV2DP4P0WH&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=VYU22TH6U14&afp2=oa' });
     res.end();
   }
