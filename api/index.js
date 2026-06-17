bdc80b49f300a948d67e66c05d797c7afa6ff86c
JCPrime01 <vanzokiller@gmail.com>
2026-06-17 13:03:03 -0300

    chore: shareCode JS2KDLY6W2M -> HU1CSAYLN8R (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=JS2KDLY6W2M&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=HU1CSAYLN8R&afp2=oa' });
     res.end();
   }
