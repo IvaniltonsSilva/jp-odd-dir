5ae4725debda961b08a4b10b026ccb966bcea59a
JCPrime01 <vanzokiller@gmail.com>
2026-07-19 11:58:41 -0300

    chore: shareCode H1EGPYMUN46 -> YDGOOQ10UZZ (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=H1EGPYMUN46&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=YDGOOQ10UZZ&afp2=oa' });
     res.end();
   }
