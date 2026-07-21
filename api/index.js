40cc043ef34d167743036e50d5d4681d6d0d4347
JCPrime01 <vanzokiller@gmail.com>
2026-07-21 14:25:01 -0300

    chore: shareCode YDGOOQ10UZZ -> PK84LI1U13U (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=YDGOOQ10UZZ&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=PK84LI1U13U&afp2=oa' });
     res.end();
   }
