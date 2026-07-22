26f59bb9e68bed894fce0d6113fc0e5a300f6ad8
JCPrime01 <vanzokiller@gmail.com>
2026-07-22 15:35:55 -0300

    chore: shareCode PK84LI1U13U -> 6E1YAH3SM0F (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=PK84LI1U13U&afp2=oa' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/6az74fb9?shareCode=6E1YAH3SM0F&afp2=oa' });
     res.end();
   }
